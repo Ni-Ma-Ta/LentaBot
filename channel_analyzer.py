908098423568b32d37d59a46627c74e24f851c7b
Maxim <maxim@89-108-99-18.cloudvps.regruhosting.ru>
2019-06-08 17:24:01 +0300

    Changing format of returning data

--- channel_analyzer.py
+++ channel_analyzer.py
@@ -3,10 +3,14 @@ from secrets import telegram_api_id as api_id, \
         telegram_api_hash as api_hash
 
 def get_interesting_messages(chat_id, count, time_limit):
-    with telethon.TelegramClient('NiMaTaLentaBot', api_id, api_hash) as client:
-        message = client.iter_messages(chat_id, limit=count)
-    return message    
+    messages = []
+    with telethon.TelegramClient('NiMaTaLentaBot', api_id, api_hash) as client: 
+        for message in client.iter_messages(chat_id, limit=count):
+            messages.append('{} : {}'.format(message.date, message.views))         
+    return messages
 
 if __name__ == "__main__":
-    print(get_interesting_messages('@Cbpub', 5, 3))
+    data = get_interesting_messages('@Cbpub', 5, 3)
+    for iter in data:
+        print(iter)
 
