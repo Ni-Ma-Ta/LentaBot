1b7e7ab06b707dadd53ee4dba091d56c98d138ec
Maxim <maxim@89-108-99-18.cloudvps.regruhosting.ru>
2019-06-08 23:08:11 +0300

    First version of analyzer with docs

--- channel_analyzer.py
+++ channel_analyzer.py
@@ -1,7 +1,7 @@
 import telethon.sync as telethon
 from secrets import telegram_api_id as api_id, \
         telegram_api_hash as api_hash
-
+import operator, datetime
 class MessagesCollector:
     def __init__(self):
         self.client = telethon.TelegramClient('NiMaTaLentaBot', api_id, api_hash)
@@ -11,14 +11,30 @@ class MessagesCollector:
         self.client.disconnect()
 
     def get_interesting_messages(self, chat_id, count, time_limit):
-        messages = []
+        '''
+        @param {str} chat_id A string, which contain chat share link
+        @param {int} count Amount of the most popular messages, which ids will be return
+        @param {int} time_limit Amount of hours, so messages, which have been send after current time - this amount of hours, will be included into comparison  
+        @returns {list} List, which contains ids, which belong to most popular messsages in this chat 
+        '''
         data = {}
-        for message in self.client.iter_messages(chat_id, limit=count):
-            messages.append(message.id)
-            data[message.id] = message.date
+        today = datetime.datetime.utcnow()
+        if today.hour - time_limit < 0:
+            limit_date = datetime.datetime(hour=today.hour-time_limit + 24, minute=today.minute, second=today.second,  \
+                day=today.day - 1, month=today.month, year=today.year, tzinfo=datetime.timezone.utc)
+        else:
+            limit_date = datetime.datetime(hour=today.hour-time_limit, minute=today.minute, second=today.second,  \
+                day=today.day, month=today.month, year=today.year, tzinfo=datetime.timezone.utc)
+        for message in self.client.iter_messages(chat_id, offset_date = limit_date, reverse=True):
+            if message.date >= limit_date:
+                data[message.id] = message.views
+        sorted_data = sorted(data.items(), key=operator.itemgetter(1))
+        sorted_data.reverse()
+        sorted_data = sorted_data[:count]
+        messages = [i[0] for i in sorted_data]
         return messages
 
 if __name__ == "__main__":
     a =  MessagesCollector()
-    data = a.get_interesting_messages('@Cbpub', 5, 3)
+    data = a.get_interesting_messages('@Cbpub', 5, 24)
     print(data)
