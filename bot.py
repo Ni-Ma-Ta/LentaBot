b17743b5e5ab83da316f5bc465b0f8fa8a5546df
Tanya <tanya@89-108-99-18.cloudvps.regruhosting.ru>
2019-06-08 16:07:46 +0300

    Bot was connected with channels_handler

--- bot.py
+++ bot.py
@@ -1,15 +1,21 @@
 import telebot
 
 from secrets import telebot_token
+from channels_handler import ChannelsHandler
 
 bot = telebot.Telebot(telebot_token)
+all_users = dict()
 
 @bot.message_handler(commands=['start'])
 def init(message):
-    bot.reply_to(message, 'Привет! Я симулирую ленту наиболее популярных новостей по выбранным вами каналам. \nДля того, чтобы начать, пришлите мне "Добавить новый канал [Название канала] [Частота в часах] [Количество новостей]". Я начну присылать вам новости по этому каналу так часто, как вы мне скажете. \n\nЧтобы посмотреть полный список команд, пришлите /help')
+    if message.chat.id in all_users:
+        bot.reply_to(message, 'Вы уже зарегистрированы. Если вы хотите сбросить все настройки, то пришлите /stop')
+    else:
+        all_users[message.chat.id] = ChannelsHandler(bot, message.chat.id)
+        bot.reply_to(message, 'Привет! Я симулирую ленту наиболее популярных новостей по выбранным вами каналам. \nДля того, чтобы начать, пришлите мне "Добавить новый канал [Название канала] [Частота в часах] [Количество новостей]". Я начну присылать вам новости по этому каналу так часто, как вы мне скажете. \n\nЧтобы посмотреть полный список команд, пришлите /help')
 
 @bot.message_handler(commands=['help'])
-def init(message):
+def comands(message):
     bot.reply_to(message, 'Полный список команд: \n"Добавить новый канал [Название канала] [Частота в часах] [Количество новостей]" - Я начну присылать вам выбранное количество самый популярных новостей выбранного канала каждое выбранное количество часов. Отсчет времени начнется с момента добавления нового канала. \n"Удалить канал [Название канала] - Я больше не буду присылать вам новости этого канала. \n"Изменить количество новостей канала [Название канала] [Количество новостей]" - Теперь я буду присылать вам другое количество новостей по этому каналу. \n"Изменить частоту получения новостей канала [Название канала] [Частота в часах]" - Я буду присылать вам новости этого канала с измененной частотой.')
 
 def is_int(s):
@@ -27,21 +33,21 @@ def is_int(s):
 @bot.message_handler()
 def msg_handler(message):
     text = message.text.lower()
-    if(" ".join(text.split()[0:3]) = "добавить новый канал" and is_int(text.split()[-1])\
-            and is_int(text.split()[-2]) and text.split()[0:-2] != "добавить новый канал"):
+    if(len(text.split() == 6 and " ".join(text.split()[0:3]) = "добавить новый канал" and is_int(text.split()[4])\
+            and is_int(text.split()[5])):
         #добавляем новый канал
-
-    elif(" ".join(text.split()[0:2]) = "удалить канал" and text != "удалить канал"):
+        all_users[message.chat.id].add_channel(text.split()[3], int(text.split()[4]), int(text.split()[5]))
+    elif(len(text.split()) == 3 and " ".join(text.split()[0:2]) = "удалить канал"):
         #удаляем канал
-
-    elif(" ".join(text.split()[0:4]) = "изменить количество новостей канала" \
-	        and " ".join(text.split()[0:-1]) != "изменить количество новостей канала" and is_int(text.split()[-1])):
+        all_users[message.chat.id].del_channel(text.split()[2])
+    elif(len(text.split()) == 6 and " ".join(text.split()[0:4]) = "изменить количество новостей канала"\
+            and is_int(text.split()[5])):
         #изменяем количество новостей
-
-    elif(" ".join(text.split()[0:5]) = "изменить частоту получения новостей канала" \
-	        and " ".join(text.split()[0:-1]) != "изменить частоту получения новостей канала" and is_int(text.split()[-1])):
+        all_users[message.chat.id].edit_channel(text.split()[4], new_count = int(text.split()[-1])
+    elif(len(text.split()) == 7 and " ".join(text.split()[0:5]) = "изменить частоту получения новостей канала" \
+	    and is_int(text.split()[-1])):
         #изменяем частоту
-
+        all_users[message.chat.id].edit_channel(text.split()[5], new_frequency = int(text.split()[6]))
     else:
         bot.reply_to(message, 'Неверный формат. Попробуйте /help')
 
