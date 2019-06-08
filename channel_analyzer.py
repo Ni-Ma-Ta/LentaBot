1b6d930333a75e7fc2c143546c5801e26bea8b64
Maxim <maxim@89-108-99-18.cloudvps.regruhosting.ru>
2019-06-08 18:45:40 +0300

    bugs with threads are fixed

--- channel_analyzer.py
+++ channel_analyzer.py
@@ -2,15 +2,25 @@ import telethon.sync as telethon
 from secrets import telegram_api_id as api_id, \
         telegram_api_hash as api_hash
 
-def get_interesting_messages(chat_id, count, time_limit):
-    messages = []
-    with telethon.TelegramClient('NiMaTaLentaBot', api_id, api_hash) as client: 
-        for message in client.iter_messages(chat_id, limit=count):
-            messages.append('{} : {}'.format(message.date, message.views))         
-    return messages
+class MessagesCollector:
+    def __init__(self):
+        self.client = None
+
+    def __enter__(self):
+        self.client = telethon.TelegramClient('NiMaTaLentaBot', api_id, api_hash)
+        self.client.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_value, tb):
+        self.client.disconnect()
+
+    def get_interesting_messages(self, chat_id, count, time_limit):
+        messages = []
+        for message in self.client.iter_messages(chat_id, limit=count):
+            messages.append(message.id)         
+        return messages
 
 if __name__ == "__main__":
-    data = get_interesting_messages('@Cbpub', 5, 3)
-    for iter in data:
-        print(iter)
-
+    with MessagesCollector() as a:
+        data = a.get_interesting_messages('@Cbpub', 5, 3)
+        print(data)
