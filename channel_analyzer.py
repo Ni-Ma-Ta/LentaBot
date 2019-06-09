a83b325f7923eb58849de7a7d6481987a814faef
Maxim <maxim@89-108-99-18.cloudvps.regruhosting.ru>
2019-06-09 14:30:53 +0300

    Float numbers had been added

--- channel_analyzer.py
+++ channel_analyzer.py
@@ -33,12 +33,16 @@ class MessagesCollector:
         all_messages = []
         data = {}
         today = datetime.datetime.utcnow()
+        limit_date = today - datetime.timedelta(hours=int(time_limit), minutes=round((time_limit - int(time_limit)) * 60))
+        limit_date = limit_date.replace(tzinfo=datetime.timezone.utc)
+        '''
         if today.hour - time_limit < 0:
             limit_date = datetime.datetime(hour=today.hour-time_limit + 24, minute=today.minute, second=today.second,  \
                 day=today.day - 1, month=today.month, year=today.year, tzinfo=datetime.timezone.utc)
         else:
             limit_date = datetime.datetime(hour=today.hour-time_limit, minute=today.minute, second=today.second,  \
                 day=today.day, month=today.month, year=today.year, tzinfo=datetime.timezone.utc)
+        '''
         for message in self.client.iter_messages(chat_id, offset_date=limit_date, reverse=True):
             data[message.id] = message.views
             all_messages.append(message)
@@ -50,5 +54,5 @@ class MessagesCollector:
 
 if __name__ == "__main__":
     a =  MessagesCollector()
-    data = a.get_interesting_messages('@Cbpub', 5, 24)
+    data = a.get_interesting_messages('@Cbpub', 5, 0.017)
     print(data)
