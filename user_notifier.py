11e5931ebe25ec63c969f62af325608fedbbd4f4
Nikolay Nechaev <Nikolay_Nechaev@mail.ru>
2019-06-09 19:06:14 +0300

    User notifier

---
+++ user_notifier.py
@@ -0,0 +1,18 @@
+  
+import telebot
+import pickle
+from secrets import telebot_token as token
+from sys import stdin
+
+bot = telebot.TeleBot(token)
+print("Let's start. To interrupt the process, type ^C")
+print("I'm going to send your message to all the users. Type the message. To finish message, type ^D (for linux) or ^Z (for windows). To cancel this, type ^C\n")
+print("\nFormatting style is Telegram Markdown")
+message = stdin.read()
+print()
+with open("userdata.pickle", "rb") as f:
+    for user_id in pickle.load(f).keys():
+        try:
+            bot.send_message(user_id, message, parse_mode='Markdown')
+        except:
+            print("Couldn't send message to {}".format(user_id))
